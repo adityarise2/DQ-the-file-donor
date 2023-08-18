64c5e31c85f5ca910842686c7705acf6ec526d78
adityarise2 <137521434+adityarise2@users.noreply.github.com>
2023-08-18 17:51:51 +0530

    pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -66,7 +66,7 @@ async def give_filter(client, message):
                 parse_mode=enums.ParseMode.HTML
             )
 
-@Client.on_message(filters.private & filters.text & filters.incoming)
+@Client.on_message(filters.private | filters.text & filters.incoming)
 async def pm_text(bot, message):
     content = message.text
     user = message.from_user.first_name
